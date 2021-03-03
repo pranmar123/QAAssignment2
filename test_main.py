1dfb1e30b81eacccdf75ecc50347378ac5af812c
pranmar123 <pranmar123@gmail.com>
2021-03-03 04:00:36 -0600

    Add menu

--- test_main.py
+++ test_main.py
@@ -1,12 +1,7 @@
 import unittest
 from main import *
 
-#establish 3 testing classes, one for the Menu, one to test the BMI module, and the last to test the retirement module
-
-class TestMenu(unittest.TestCase):
-    def test_intro(self):
-        expected = "Welcome to the BMI/Savings Program"
-        self.assertEqual(intro(),expected)
+#establish two different classes for tests: one to test the BMI module, and the last to test the retirement module
 
 class TestBMI(unittest.TestCase):
     pass
@@ -14,6 +9,5 @@ class TestBMI(unittest.TestCase):
 class TestRetirement(unittest.TestCase):
     pass
 
-
-if __name__ == '__main__': 
-    unittest.main() 
+if __name__ == '__main__':
+    unittest.main()
