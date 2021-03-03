f59e09dc0792750e68b7aab313e605b15264edf8
pranmar123 <pranmar123@gmail.com>
2021-03-03 02:53:53 -0600

    Add test imports and link to main.py

---
+++ test_main.py
@@ -0,0 +1,13 @@
+import unittest
+from main import *
+
+#establish 3 testing classes, one for the Menu, one to test the BMI module, and the last to test the retirement module
+
+class TestMenu(unittest.TestCase):
+    pass
+
+class TestBMI(unittest.TestCase):
+    pass
+
+class TestRetirement(unittest.TestCase):
+    pass
